5a0bcf8281bad28c034a20e8e22faa4110c6a7a2
René Kijewski <rene.kijewski@fu-berlin.de>
2021-11-23 23:43:32 +0100

    .

--- examples/fullscreen.py
+++ examples/fullscreen.py
@@ -6,16 +6,20 @@ from pyiced import (
 class FullscreenExample(IcedApp):
     def __init__(self):
         self.__fullscreen = False
+        self.__should_exit = False
 
     class settings:
         class window:
             size = (640, 320)
 
+    def subscriptions(self):
+        return [Subscription.UNCAPTURED]
+
     def fullscreen(self):
         return self.__fullscreen
 
-    def subscriptions(self):
-        return [Subscription.UNCAPTURED]
+    def should_exit(self):
+        return self.__should_exit
 
     def title(self):
         return self.__message
@@ -24,6 +28,8 @@ class FullscreenExample(IcedApp):
         match msg:
             case Message(keyboard='keyreleased', key_code='F11'):
                 self.__fullscreen = not self.__fullscreen
+            case Message(keyboard='keyreleased', key_code='Escape'):
+                self.__should_exit = True
 
     def view(self):
         return container(
